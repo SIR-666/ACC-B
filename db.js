77e810d3e09b20c5d4347d4de2c4f5ab0b6b9f53
Syahroni Isron Ramdana ( Automation Technician - Manufacturing - Dairy Indonesia ) <10080143@greenfields.id>
2025-11-18 13:36:16 +0700

    ganti host

--- db.js
+++ db.js
@@ -1,18 +1,23 @@
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
-    host: "202.10.43.21",
-    user: "accm5651_caksyu",
-    password: "970008F#m",
-    database: "accm5651_Accounting"
+// WAJIB pakai pool di shared hosting
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: "202.10.43.21",
+  user: "accm5651_caksyu",
+  password: "970008F#m",
+  database: "accm5651_Accounting",
+  port: 3306
 });
 
-db.connect(err => {
-    if (err) {
-        console.error("Koneksi gagal:", err);
-    } else {
-        console.log("MySQL Connected!");
-    }
+// Cek koneksi pertama
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("Koneksi gagal:", err);
+  } else {
+    console.log("MySQL Connected!");
+    connection.release();
+  }
 });
 
 module.exports = db;
