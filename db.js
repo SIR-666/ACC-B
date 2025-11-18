f35402bc46956f054f80a73e593ef2ace5a471d8
Syahroni Isron Ramdana ( Automation Technician - Manufacturing - Dairy Indonesia ) <10080143@greenfields.id>
2025-11-18 13:32:48 +0700

    ganti host

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "localhost",
+    host: "202.10.43.21",
     user: "accm5651_caksyu",
     password: "970008F#m",
     database: "accm5651_Accounting"
